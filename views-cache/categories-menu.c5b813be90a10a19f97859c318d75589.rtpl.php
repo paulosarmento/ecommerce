fa9a9370a39e226142ae9633af18a5be08d29d56
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-03-23 19:48:07 -0300

    Categoria Vs Produtos

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/3">Android </a></li><li><a href="/categories/7">Apple</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/8">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/7">Apple</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/8">Samsung</a></li><li><a href="/categories/13">sport</a></li>
