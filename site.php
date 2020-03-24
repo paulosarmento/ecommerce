0c26c0fb2881c309476a037ced525416755850c7
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-03-24 19:14:23 -0300

    Carrinho - Calculo do frete

--- site.php
+++ site.php
@@ -76,7 +76,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 
 	//var_dump($cart);
@@ -128,6 +129,16 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	$cart = Cart::getFromSession();
 
 	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
 
 	header("Location: /cart");
 	exit;
