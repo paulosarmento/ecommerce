df501d264c2b91b8d34afac186657d66b10b5293
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-03-24 09:25:17 -0300

    Detalhes do Produto

--- site.php
+++ site.php
@@ -48,8 +48,27 @@ $app->get("/categories/:idcategory", function($idcategory)
 		'pages'=>$pages
 
 	]);
+	
+});
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+
+	]);
+
+
 
 });
+
 
 
 ?>
