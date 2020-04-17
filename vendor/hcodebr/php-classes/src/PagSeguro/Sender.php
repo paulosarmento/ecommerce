7488eae0df2760bd5c64243162048e01111726b2
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-04-17 11:49:16 -0300

    Classe Sender

--- vendor/hcodebr/php-classes/src/PagSeguro/Sender.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Sender.php
@@ -13,6 +13,76 @@ class Sender{
     private $hash;
     private $ip;
     
+    public function __construct(
+        string $name, 
+        Document $cpf, 
+        DateTime $bornDate, 
+        Phone $phone,
+        string $email,
+        string $hash
+        )
+    {
+        if(!$name)
+        {
+            throw new Exception("Informe o nome do comprador");
+        }
+        if(!filter_var($email, FILTER_VARLIDATE_EMAIL))
+        {
+            throw new Exception("O e-mail informado não é válido");
+        }
+        if(!$hash)
+        {
+            throw new Exception("Informe o identificação do comprador");
+        }
+
+        $this->name = $name;
+        $this->cpf = $cpf;
+        $this->bornDate = $bornDate;
+        $this->phone = $phone;
+        $this->email = $email;
+        $this->hash = $hash;        
+        $this->ip = $_SERVER["REMOTE_ADDR"];
+
+    }
+
+    public function getDomElement():DOMElement
+   {
+
+       $dom = new DOMDocument();
+
+       $sender = $dom->createElement("sender");
+       $sender = $dom->appendChild($sender);
+
+       $name = $dom->createElement("name", $this->name);
+       $name = $sender->appendChild($name);
+
+       $email = $dom->createElement("email", $this->email);
+       $email = $sender->appendChild($email);
+
+       $bornDate = $dom->createElement("bornDate", $this->bornDate->format("d/m/y"));
+       $bornDate = $sender->appendChild($bornDate);
+
+       $documents = $dom->createElement("documents", $this->documents);
+       $documents = $sender->appendChild($documents);
+
+       $cpf = $this->cpf->getDOMElement();
+       $cpf = $dom->importNode($cpf, true);
+       $cpf = $documents->appendChild($cpf);
+
+       $phone = $this->phone->getDOMElement();
+       $phone = $dom->importNode($phone, true);
+       $phone = $documents->appendChild($phone);
+
+       $hash = $dom->createElement("hash", $this->hash);
+       $hash = $sender->appendChild($hash);
+
+       $ip = $dom->createElement("ip", $this->ip);
+       $ip = $sender->appendChild($ip);
+       
+
+       return $sender;
+
+   }
 
 }
 
