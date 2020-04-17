f2c54a3cd5d9763a826ea2b3f3730ef6686292eb
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-04-17 11:11:44 -0300

    Classe phone

--- vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
@@ -6,7 +6,40 @@ class Phone{
  
     private $areaCode;
     private $number;
-   
+
+    public function __construct(int $areaCode, int $number)
+    {
+
+        if(!$areaCode || $areaCode < 11 || $areaCode > 99)
+        {
+            throw new Exception("Informe o DDD do telefone.");
+        }
+        if(!$areaCode || strlen < 11 || $areaCode > 99)
+        {
+            throw new Exception("Informe o número do telefone");
+        }
+
+        $this->areaCode = $areaCode;
+        $this->number = $number;
+
+    }
+   public function getDomElement():DOMElement
+   {
+
+       $dom = new DOMDocument();
+
+       $phone = $dom->createElement("phone");
+       $phone = $dom->appendChild($phone);
+
+       $areaCode = $dom->createElement("are$areaCode", $this->$areaCode);
+       $areaCode = $phone->appendChild($areaCode);
+
+       $number = $dom->createElement("number", $this->number);
+       $number = $phone->appendChild($number);
+
+       return $phone;
+
+   }
 
 }
 
